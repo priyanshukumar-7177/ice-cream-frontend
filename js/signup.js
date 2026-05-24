1885301c95269176c5424aadf82ccb469fd4391f
priyanshukumar-7177 <sagarpriyanshu6287@gmail.com>
2026-05-24 21:53:39 +0530

    Update BASE_URL to production server

--- js/signup.js
+++ js/signup.js
@@ -10,7 +10,7 @@
    REGISTRATION PAGE
 ══════════════════════ */
 
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://ice-cream-backend-zwrr.onrender.com";
 
 
 // Input fields
