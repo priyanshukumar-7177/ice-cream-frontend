d660dda91b8eb5e0e11cb064e625481b444a5029
priyanshukumar-7177 <sagarpriyanshu6287@gmail.com>
2026-05-24 21:52:39 +0530

    Update BASE_URL to production endpoint

--- js/order-success.js
+++ js/order-success.js
@@ -1,6 +1,6 @@
 const $ = id => document.getElementById(id);
 
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://ice-cream-backend-zwrr.onrender.com";
 
 
 /* =============================================================
