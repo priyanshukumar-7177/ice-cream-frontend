f05323abe253b880da0f652a3488ec3d9fe15c1d
priyanshukumar-7177 <sagarpriyanshu6287@gmail.com>
2026-05-24 22:05:29 +0530

    Update BASE_URL to include API version

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ const loginPasswordInput = document.querySelector('input[type="password"]');
 const loginSuccessBox = document.getElementById("loginSuccessBox");
 const loginFormSection = document.getElementById("loginFormSection");
 
-const BASE_URL = "https://ice-cream-backend-zwrr.onrender.com";
+const BASE_URL = "https://ice-cream-backend-zwrr.onrender.com/api/v1";
 
 
 async function loginUser() {
